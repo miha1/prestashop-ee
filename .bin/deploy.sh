cc57e5b297d0a58045f364258d6d9c60ba335434
Mihael Lovrencic <mihael.lovrencic@wirecard.com>
2019-07-01 09:58:05 +0200

    Fix: try to make a release

---
+++ .bin/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+git remote rm origin
+git remote add origin https://miha1:${GITHUB_TOKEN}@github.com/miha1/$DEPLOYMENT_PROJECT.git
+git symbolic-ref HEAD refs/heads/master
+
+npm i -g npm
+npm install --only=dev
+npx release-it --ci -eV -V
