419a88fbbc9a109216427ae8b933b49b1f99f836
Mihael Lovrencic <mihael.lovrencic@wirecard.com>
2019-07-01 15:01:11 +0200

    fix: try 9

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -3,6 +3,8 @@ git remote rm origin
 git remote add origin https://miha1:${GITHUB_TOKEN}@github.com/miha1/prestashop-ee.git
 git symbolic-ref HEAD refs/heads/master
 
+bash .bin/generate-release-package.sh
+
 npm i -g npm
 npm install --only=dev
 npm run release --ci -eV
